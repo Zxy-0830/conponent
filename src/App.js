53a7801dd293fb1653508f099f1a90d47a4e986e
ZXY-0830 <3274592096@qq.com>
2025-05-15 01:41:47 -0500

    commit for indexcss

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     {"id": "03", "img": "https://raw.githubusercontent.com/Zxy-0830/photos/refs/heads/main/photos/img-03.jpg"},
     {"id": "04", "img": "https://raw.githubusercontent.com/Zxy-0830/photos/refs/heads/main/photos/img-04.jpg"}
   ]
-  const title = 'title';
-  const content = 'contant';
+  const title = 'Joker Xue: From Setbacks to Stardom';
+  const content = 'Joker Xue is a Chinese singer-songwriter who turned early setbacks into major success. After debuting in 2005, he was once sidelined by his label and faded from the spotlight. Refusing to give up, he focused on songwriting and made a strong comeback with hits like Actor, What Else Do You Want Me To Do, and The Crossing. Now recognized as one of Mainland China’s top male artists, Xue is known for his emotional lyrics and captivating performances. is third concert tour, Extraterrestrial Objects, broke attendance records across China, confirming his status as a true pop icon.';
   return (
     <main>
       <>
